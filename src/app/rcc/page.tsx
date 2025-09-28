df69a19c0606887544fab52b564c4eeb098d4695
AkitoSaito <akito136@icloud.com>
2025-09-28 22:33:08 +0900

    ClientComponentにAboutリンクを追加し、ServerComponentにAboutリンクを追加

--- src/app/rcc/page.tsx
+++ src/app/rcc/page.tsx
@@ -1,12 +1,17 @@
 "use client";
 import { useState } from "react";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 export default function ClientComponent() {
   const [count, setCount] = useState(0);
+  const router = useRouter();
   return (
     <div>
       クライアント
       <button onClick={() => setCount(count + 1)}>Count: {count}</button>
+      <button onClick={() => router.push("about")}>Aboutへ</button>
+      <Link href="/">Homeへ</Link>
     </div>
   );
 }
