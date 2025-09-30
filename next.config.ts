e985e7ed755f24d85b19763833035ddb00d6da93
AkitoSaito <akito136@icloud.com>
2025-09-30 21:49:11 +0900

    next.config.tsに画像のリモートパターンを追加

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
+// 外部APIを利用する場合はnext.config.jsでドメインを許可する必要がある
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.dog.ceo",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
