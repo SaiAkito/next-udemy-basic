51c3c9e6899d006097b4446072c526a10b6e4007
AkitoSaito <akito136@icloud.com>
2025-10-05 22:39:05 +0900

    close

--- src/lib/actions/contact.ts
+++ src/lib/actions/contact.ts
@@ -3,6 +3,7 @@
 import { redirect } from "next/navigation";
 
 import { contactSchema } from "@/validations/contact";
+import { prisma } from "@/lib/prisma";
 
 // ActionStateの型定義
 type ActionState = {
@@ -15,8 +16,8 @@ type ActionState = {
 };
 
 export async function submitContactForm(prevState: ActionState, formData: FormData): Promise<ActionState> {
-  const name = formData.get("name");
-  const email = formData.get("email");
+  const name = formData.get("name") as string;
+  const email = formData.get("email") as string;
 
   // バリデーション
   const validationResult = contactSchema.safeParse({ name, email });
@@ -30,6 +31,23 @@ export async function submitContactForm(prevState: ActionState, formData: FormDa
   }
 
   // DB登録
+  // メールアドレスの存在確認
+  const existingRecord = await prisma.contact.findUnique({ where: { email: email } });
+
+  if (existingRecord) {
+    return {
+      success: false,
+      error: { name: [], email: ["このメールアドレスは既に登録されています。"] },
+    };
+  }
+
+  // 登録処理
+  await prisma.contact.create({
+    data: {
+      name,
+      email,
+    },
+  });
 
   console.log("送信されたデータ:", name, email);
   redirect("/contacts/complete");
